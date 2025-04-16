96c3aeef34638bd6b5e27f646b12e30b2ea8aec9
JJGL-2264422 <jjgl14ag03@gmail.com>
2025-04-16 14:37:24 -0500

    [+] Añadido el cambio de color dependiendo del estado del cronometro (Activo/Inactivo) [+] Comentarios extras añadidos [+] Acreditación del código que permite arrastrar la ventana

--- cronometro.py
+++ cronometro.py
@@ -1,8 +1,8 @@
 import tkinter as tk
 import win32gui as wui
 
+#Código de Ventana Arrastrable creado por James Kent
 class Grip:
-    ''' Makes a window dragable. '''
     def __init__ (self, parent, disable=None, releasecmd=None) :
         self.parent = parent
         self.root = parent.winfo_toplevel()
@@ -41,37 +41,47 @@ class Grip:
         if self.releaseCMD != None :
             self.releaseCMD()
 
+
 def update():
     global startTime, winCode, timer, AppName, ScoreL
     winCode = wui.GetForegroundWindow() #Toma el código de la aplicación activa
     winTitle = wui.GetWindowText(wui.GetForegroundWindow()) #Debug - Toma el título de la ventana activa
     timer.title(winTitle)
-    AppName.configure(text=f"AppCode: {winCode}")
-    if(winCode == 788944):
+    AppName.configure(text=f"AppCode: {winCode}") #Debug
+    if(winCode == 788944): #Cambiar por la lista de códigos enlazados a la tarea
         startTime += 1
         seconds = startTime % 60
         minutes = int(startTime / 60) % 60
         hours = int(startTime / 3600)
-        ScoreL.configure(text=f"{hours:02}:{minutes:02}:{seconds:02}")
+        ScoreL.configure(text=f"{hours:02}:{minutes:02}:{seconds:02}",bg='lightgreen') #Cambia el color a verde mientras está activo
+    else: #Cambia el color del fondo a rojo si está inactivo
+        ScoreL.configure(bg='#f54257')
     timer.after(1000, update)
+
+# [!] Solo es necesario cambiar el color del label del tiempo, ya que abarca toda la ventana.
 
 def main():
     global startTime, winCode, timer, AppName, ScoreL
     timer = tk.Tk()
     winCode = 0
+    startTime = 0 #Se cambia por el valor guardado de Total de horas trabajadas
+    seconds = startTime % 60
+    minutes = int(startTime / 60) % 60
+    hours = int(startTime / 3600)
+
     timer.title('-')
     timer.wm_attributes("-topmost", True)
-    startTime = 0 #Se cambia por el valor guardado
+    #timer.configure(bg='#f54257')
 
     AppName = tk.Label(timer, text=f"App Code: {winCode}") #Debug - Solo para mostrar el código de la aplicación activa
     AppName.pack()
-    ScoreL = tk.Label(timer, text="00:00:00", font=('Lexend',20))
+    ScoreL = tk.Label(timer, text=f"{hours:02}:{minutes:02}:{seconds:02}", font=('Lexend',20), bg='#f54257')
     ScoreL.pack()
 
     timer.overrideredirect(True)
-    grip = Grip(timer)
+    grip = Grip(timer) #Hace arrastable la ventana del cronometro.
 
-    timer.after(1000, update) # start the update 1 second later
+    timer.after(1000, update)
     timer.mainloop()
 
 main()
