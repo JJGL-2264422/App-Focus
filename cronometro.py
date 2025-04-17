3c767f1896e20432f339341bedfeeeb819e61672
JJGL-2264422 <jjgl14ag03@gmail.com>
2025-04-17 17:00:57 -0500

    [+] Ahora se toma el nombre de la aplicación utilizando su ThreadID [+] El condicional del Timer ahora utiliza el nombre de la aplicación

--- cronometro.py
+++ cronometro.py
@@ -1,5 +1,10 @@
+import time
+import psutil
 import tkinter as tk
 import win32gui as wui
+import win32process as winp
+
+active = True
 
 #Código de Ventana Arrastrable creado por James Kent
 class Grip:
@@ -44,11 +49,12 @@ class Grip:
 
 def update():
     global startTime, winCode, timer, AppName, ScoreL
-    winCode = wui.GetForegroundWindow() #Toma el código de la aplicación activa
-    winTitle = wui.GetWindowText(wui.GetForegroundWindow()) #Debug - Toma el título de la ventana activa
+    winCode = winp.GetWindowThreadProcessId(wui.GetForegroundWindow()) #Toma el código de la aplicación activa
+    winName = psutil.Process(winCode[-1]).name() #Toma el nombre de la aplicación a partir del código
+    winTitle = winp.GetWindowThreadProcessId(wui.GetForegroundWindow()) #Debug - Toma el título de la ventana activa
     timer.title(winTitle)
-    AppName.configure(text=f"AppCode: {winCode}") #Debug
-    if(winCode == 788944): #Cambiar por la lista de códigos enlazados a la tarea
+    AppName.configure(text=f"AppCode: {winName}") #Debug
+    if(winName == 'Code.exe'): #Cambiar por la lista de procesos enlazados a la tarea
         startTime += 1
         seconds = startTime % 60
         minutes = int(startTime / 60) % 60
@@ -58,10 +64,12 @@ def update():
         ScoreL.configure(bg='#f54257')
     timer.after(1000, update)
 
+            
+
 # [!] Solo es necesario cambiar el color del label del tiempo, ya que abarca toda la ventana.
 
-def main():
-    global startTime, winCode, timer, AppName, ScoreL
+def openTimer():
+    global startTime, winCode, timer, AppName, ScoreL, active
     timer = tk.Tk()
     winCode = 0
     startTime = 0 #Se cambia por el valor guardado de Total de horas trabajadas
@@ -83,5 +91,3 @@ def main():
 
     timer.after(1000, update)
     timer.mainloop()
-
-main()
