7ba067397e13cab616a5808cf536f7badf9245ac
JJGL-2264422 <jjgl14ag03@gmail.com>
2025-04-22 13:04:48 -0500

    [+] Se añadieron los campos "horasObj" y "programa" [!] La ventana principal se convirtió en la definición "dashboard"

--- tarea.py
+++ tarea.py
@@ -3,11 +3,34 @@ from tkinter import simpledialog, messagebox, ttk
 import json
 import os
 from datetime import datetime
+#import login
 
 TAREAS_FILE = 'tareas.json'
 
+# Ventana principal
+def dashboard(usuario):
+    global root 
+    root = tk.Tk()
+    root.title("Bienvenido")
+    root.geometry("1200x800")
+
+    frame = tk.Frame(root)
+    frame.place(relx=0.5, rely=0.5, anchor="center")
+
+    tk.Label(frame, text=f"¡Bienvenido, {usuario}!", font=("Arial", 32),height=2).pack(pady=40)
+    #tk.Button(frame, text="Cerrar sesión", font=("Arial", 18), width=20,
+    #          command=lambda: login.cerrar_sesion(root)).pack(pady=20)
+
+    btn_crear = tk.Button(frame, text="Crear tarea", command=crear_tarea, width=30, height=2)
+    btn_crear.pack(pady=20)
+
+    btn_ver = tk.Button(frame, text="Ver tareas", command=mostrar_tareas, width=30, height=2)
+    btn_ver.pack(pady=20)
+
+    root.mainloop()
+
 # Guardar tarea con fecha y hora actual
-def guardar_tarea(nombre):
+def guardar_tarea(nombre, horaObj, programa):
     if not nombre:
         return
 
@@ -19,6 +42,8 @@ def guardar_tarea(nombre):
         "nombre": nombre,
         "fecha": fecha,
         "hora": hora,
+        "horaObj": horaObj,
+        "programa": programa
     }
 
     if os.path.exists(TAREAS_FILE):
@@ -30,7 +55,7 @@ def guardar_tarea(nombre):
     tareas.append(tarea)
 
     with open(TAREAS_FILE, 'w') as f:
-        json.dump(tareas, f, indent=4)
+        json.dump(tareas, f, indent=6)
 
     messagebox.showinfo("Tarea guardada", f"Tarea '{nombre}' guardada exitosamente")
 
@@ -50,15 +75,15 @@ def mostrar_tareas():
     frame = tk.Frame(ventana_tareas)
     frame.place(relx=0.5, rely=0.5, anchor="center")
 
-    columnas = ("Nombre", "Fecha", "Hora")
+    columnas = ("Nombre", "Fecha", "Hora","Horas de Trabajo","Programa")
     tree = ttk.Treeview(frame, columns=columnas, show="headings", height=20)
 
     for col in columnas:
         tree.heading(col, text=col)
-        tree.column(col, width=350, anchor="center")
+        tree.column(col, width=200, anchor="center")
 
     for tarea in tareas:
-        tree.insert("", "end", values=(tarea['nombre'], tarea['fecha'], tarea['hora']))
+        tree.insert("", "end", values=(tarea['nombre'], tarea['fecha'], tarea['hora'], tarea['horaObj'], tarea['programa']))
 
     tree.pack()
 
@@ -67,20 +92,8 @@ def crear_tarea():
     
     root.geometry("1200x800")
     nombre = simpledialog.askstring("Crear tarea", "Nombre de la tarea:")
-    guardar_tarea(nombre)
+    horas = simpledialog.askstring("Crear tarea", "Horas que se desean trabajar:")
+    programa = simpledialog.askstring("Crear tarea", "Programa a utilizar:")
+    programa += ".exe"
+    guardar_tarea(nombre, horas, programa)
 
-# Ventana principal
-root = tk.Tk()
-root.title("Gestor de tareas")
-root.geometry("1200x800")
-
-frame_principal = tk.Frame(root)
-frame_principal.place(relx=0.5, rely=0.5, anchor="center")
-
-btn_crear = tk.Button(frame_principal, text="Crear tarea", command=crear_tarea, width=30, height=2)
-btn_crear.pack(pady=20)
-
-btn_ver = tk.Button(frame_principal, text="Ver tareas", command=mostrar_tareas, width=30, height=2)
-btn_ver.pack(pady=20)
-
-root.mainloop()
